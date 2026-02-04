2785e21423b408d69a7214c4cd4a1d663b48e91b
ilakiya210508-cmyk <ilakiya210508@gmail.com>
2026-02-04 19:43:17 +0530

    Create using various python print statements.py

---
+++ python programs/using various python print statements.py
@@ -0,0 +1,18 @@
+print("today is morning")
+print("today","is","morning")
+print("today","is","morning",sep="...")
+print("today","is","morning",sep="***")
+print("today","is","morning",end="!")
+print("hi","beauty")
+print("hello everyone",sep="###",end="&&")
+print("this is ilakiya",sep="$$$")
+
+
+output
+
+today is morning
+today is morning
+today...is...morning
+today***is***morning
+today is morning!hi beauty
+hello everyone&&this is ilakiya
